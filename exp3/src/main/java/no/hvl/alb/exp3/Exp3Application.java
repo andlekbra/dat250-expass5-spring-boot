8b7c81243d3f8801658c5833c7e27f09a7515fa9
framo-alb <47555931+framo-alb@users.noreply.github.com>
2021-10-08 21:03:35 +0000

    finished exp3

---
+++ exp3/src/main/java/no/hvl/alb/exp3/Exp3Application.java
@@ -0,0 +1,13 @@
+package no.hvl.alb.exp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp3Application.class, args);
+	}
+
+}
---
+++ exp3/src/test/java/no/hvl/alb/exp3/Exp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.alb.exp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
