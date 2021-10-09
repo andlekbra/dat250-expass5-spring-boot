e9b4bf8847dcb4d47e01555b3e6e2e33742dcfb2
framo-alb <47555931+framo-alb@users.noreply.github.com>
2021-10-09 05:29:23 +0000

    Finished exp4

---
+++ exp4/src/test/java/no/hvl/alb/exp4/Exp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.alb.exp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
